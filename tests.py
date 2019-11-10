1f2e6c4db9ea60e0c274654ab0e0e4ef4f44be25
heff <ksuder7@gmail.com>
2019-11-10 17:00:52 +0100

    add tests

---
+++ tests.py
@@ -0,0 +1,21 @@
+import unittest
+from blackjack import hand_score
+
+
+class TestHandScore(unittest.TestCase):
+    def test_if_jqk_gives_10_score(self):
+        self.assertEqual(hand_score(["J"]), 10)
+        self.assertEqual(hand_score(["Q"]), 10)
+        self.assertEqual(hand_score(["K"]), 10)
+
+    def test_if_card_gives_score(self):
+        self.assertEqual(hand_score(["3"]), 3)
+        self.assertEqual(hand_score(["10"]), 10)
+
+    def test_if_ace_gives_10_score_of_score_is_lt_11(self):
+        self.assertEqual(hand_score(["A"]), 11)
+
+    def test_if_ace_gives_10_score_of_score_is_gt_11(self):
+        self.assertEqual(hand_score(["A", "K", "K"]), 21)
+
+
