82d6b9085e5c55f1722c449d12da44bf6afad904
heff <ksuder7@gmail.com>
2019-11-10 17:57:04 +0100

    change tests names

--- tests.py
+++ tests.py
@@ -12,10 +12,10 @@ class TestHandScore(unittest.TestCase):
         self.assertEqual(hand_score(["3"]), 3)
         self.assertEqual(hand_score(["10"]), 10)
 
-    def test_if_ace_gives_10_score_of_score_is_lt_11(self):
+    def test_if_ace_gives_10_score_if_score_is_lt_11(self):
         self.assertEqual(hand_score(["A"]), 11)
 
-    def test_if_ace_gives_10_score_of_score_is_gt_11(self):
+    def test_if_ace_gives_1_score_if_score_is_gt_11(self):
         self.assertEqual(hand_score(["A", "K", "K"]), 21)
 
 
